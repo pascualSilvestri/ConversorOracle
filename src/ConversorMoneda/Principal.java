ca6f4bfce4887fad02a498dee10f71342902d6a4
pascual_silvestri <pascualsilvestri14@gmail.com>
2022-12-29 22:21:27 -0300

    conversor listo

--- src/ConversorMoneda/Principal.java
+++ src/ConversorMoneda/Principal.java
@@ -7,32 +7,61 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Funciones monedas = new Funciones();
+		int select;
 		
-		String opciones = (JOptionPane.showInputDialog(
-				null,
-				"Seleccione una opcion de conversion",
-				"Menu",
-				JOptionPane.QUESTION_MESSAGE,
-				null,
-				new Object[] {
-						"Conversor de moneda",
-						"Conversos de temperatura"
-					},
-				"Seleccion")
-			).toString();
 		
-		switch (opciones) {
+		do {
+			String opciones = (JOptionPane.showInputDialog(
+					null,
+					"Seleccione una opcion de conversion",
+					"Menu",
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					new Object[] {
+							"Conversor de moneda",
+							"Conversos de temperatura"
+						},
+					"Seleccion")
+				).toString();
+			switch (opciones) {
 			case "Conversor de moneda": {
-				String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
-				double valor = Double.parseDouble(input);
-				monedas.convertirMonedas(valor);
+				try {
+					String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
+					double valor = Double.parseDouble(input);
+					monedas.convertirMonedas(valor);
+				}catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Ingresar Solo numeros");
+					String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
+					double valor = Double.parseDouble(input);
+					monedas.convertirMonedas(valor);
+				}
+				break;
+				
 			}
 			case "Conversos de temperatura":{
-				String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
-				double valor = Double.parseDouble(input);
-				monedas.convertirTemperatura(valor);
+				
+				try {
+					String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
+					double valor = Double.parseDouble(input);
+					monedas.convertirTemperatura(valor);
+				}catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Ingresar Solo numeros");
+					String input = JOptionPane.showInputDialog("Ingrese un valor a convertir");
+					double valor = Double.parseDouble(input);
+					monedas.convertirTemperatura(valor);
+				}
+				
+				break;
 			}
-		}
+		}	
+			select = JOptionPane.showConfirmDialog(null, "Deseas realziar otra operacion?");
+			if(JOptionPane.YES_OPTION != select) {
+				JOptionPane.showMessageDialog(null, "Fin del Programa");
+			}
+			
+		}while(JOptionPane.YES_OPTION == select);
+		
+		
 		
 	}
 }
